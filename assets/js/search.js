a23b8fbf023b0418d5e39988909b62cdc421d5ca
dokongg <dokyung0930@naver.com>
2021-05-03 13:50:52 +0000

    Updating to dokongg/blog-management@b9c40e51adb65a86858d18753867a2ca834a3666.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,24 +1,75 @@
+var idx;
+var results;
+
+var pageEvent = function($target) {
+  var page = $target.data('page');
+  var currentPage = $('.pagination a.selected-page').data('page');
+
+  if ( currentPage == page ) {
+      return;
+  }
+
+  var initialize = false;
+
+  $('#currentPage').val(page);
+  $('.pagination a').removeClass('selected-page');
+  $target.addClass('selected-page');
+
+  if ( $target.hasClass('prev') ) {
+      initialize = true;
+      prevPageEvent();
+  }
+  if ( $target.hasClass('next') ) {
+      initialize = true;
+      nextPageEvent();
+  }
+  getPage(initialize);
+}
+
+var nextPageEvent = function() {
+  $('.pagination').empty();
+  var currentPageGroup = Number($('#currentPageGroup').val());
+  $('#currentPageGroup').val(currentPageGroup + 1);
+}
+
+var prevPageEvent = function() {
+    $('.pagination').empty();
+    var currentPageGroup = Number($('#currentPageGroup').val());
+    $('#currentPageGroup').val(currentPageGroup - 1);
+}
+
 var getPagination = function() {
     $('.pagination').empty();
-    
-    var appendHtml = '<a href="#" class="selected-page" data-page="1">1</a>';
-
     if ( $('#totalCount').val() == 0 ) {
       return;
     }
 
+    var appendHtml = '';
+    var currentPage = Number($('#currentPage').val());
+    var currentPageGroup = Number($('#currentPageGroup').val());
     var totalPageGroup = Number($('#totalPageGroup').val());
     var endPage = Number($('#endPage').val());
-    var endIndex = endPage;
+    var startIndex = ( currentPageGroup - 1 ) * 5;
 
-    if ( totalPageGroup > 1 ) {
-      endIndex = 5;
+    var endIndex = startIndex + 5 < endPage ? startIndex + 5 : endPage;
+
+    if ( currentPageGroup > 1 ) {
+      appendHtml +=  '<a href="#" data-page="' +  (startIndex) + '" class="prev">&laquo;</a>';
     }
-    for(var i = 1; i < endIndex; i ++ ) {
-      appendHtml += '<a href="#" data-page="' + (i + 1) + '">' + ( i + 1 ) + '</a>';
+
+    for(var i = startIndex; i < endIndex; i ++ ) {
+      appendHtml += '<a href="#" data-page="' + (i + 1) + '">' + (i + 1) + '</a>';
+    }
+
+    if ( totalPageGroup > 1 && currentPageGroup != totalPageGroup ) {
+      appendHtml +=  '<a href="#" data-page="' + (endIndex + 1) + '" class="next">&raquo;</a>';
     }
 
     $('.pagination').append(appendHtml);
+    $('.pagination a[data-page=' + currentPage + ']').addClass('selected-page');
+    $('.pagination a').off('click').on('click', function(e) {
+        pageEvent($(e.target));
+    });
 }
 
 var displaySearchResults = function(results, store, initalize) {
@@ -39,18 +90,16 @@ var displaySearchResults = function(results, store, initalize) {
     }
 
     var currentPage = Number($('#currentPage').val());
-    var startIndex = ( currentPage - 1 ) * 5;
+    var startIndex = (currentPage - 1) * 5;
     var endIndex = Number($('#endPage').val());
-    var currentPageGroup = Number($('#currentPageGroup').val());
-
 
     if (results.length) { // Are there any results?
         var appendString = '<div class="search-results">총 <span class="search-count">' + $('#totalCount').val() + '</span>개의 결과가 있습니다.</div>';
         for (var i = startIndex; i < results.length; i++) {  // Iterate over the results
             var item = store[results[i].ref];
-            appendString += '<li><a class="post-link" href="' + item.url + '">'
+            appendString += '<li class="post-delimiter"><a class="post-link" href="' + item.url + '">'
                             + '<h4 class="post-title">' + item.title + '</h4></a>';
-            appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
+            appendString += '<div class="search-post-contents">' + item.content + '</div></li>';
 
             if ( i == startIndex + 4 ) {
               break;
@@ -92,7 +141,7 @@ var init = function() {
       $('#searchTerm').val(searchTerm);
       // Initalize lunr with the fields it will be searching on. I've given title
       // a boost of 10 to indicate matches on this field are more important.
-      var idx = lunr(function () {
+      idx = lunr(function () {
           this.pipeline.reset();
           this.pipeline.add(
               trimmerEnKo,
@@ -114,37 +163,11 @@ var init = function() {
           });
       }
 
-      var results = idx.search(searchTerm); // Get lunr to perform a search
+      results = idx.search(searchTerm); // Get lunr to perform a search
       displaySearchResults(results, window.store, true); // We'll write this in the next section
   }
 }
 
-var getPage = function() {
-      var searchTerm  =$('#searchTerm').val();
-      // Initalize lunr with the fields it will be searching on. I've given title
-      // a boost of 10 to indicate matches on this field are more important.
-      var idx = lunr(function () {
-          this.pipeline.reset();
-          this.pipeline.add(
-              trimmerEnKo,
-              lunr.stopWordFilter,
-              lunr.stemmer
-          );
-          this.field('id');
-          this.field('title', { boost: 10 });
-          this.field('category');
-          this.field('content');
-      });
-
-      for (var key in window.store) { // Add the data to lunr
-          idx.add({
-              'id': key,
-              'title': window.store[key].title,
-              'category': window.store[key].category,
-              'content': window.store[key].content
-          });
-      }
-
-      var results = idx.search(searchTerm); // Get lunr to perform a search
-      displaySearchResults(results, window.store); // We'll write this in the next section
-    }
+var getPage = function(initialize) {
+  displaySearchResults(results, window.store, initialize);
+}
